f4d20c53569032bffd3e221be753aac162834a48
Martin Ionescu <martinionescu11@gmail.com>
2022-05-04 00:45:20 +0300

    made fields for username and password

--- front/amadeusSports/src/app/app.module.ts
+++ front/amadeusSports/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TextFieldComponent } from './text-field/text-field.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFieldComponent,
+    LogInComponent,
+    
   ],
   imports: [
+    MatFormFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
