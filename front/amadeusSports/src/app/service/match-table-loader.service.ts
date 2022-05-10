453dd6e64abfb16e8d5daa11f0341ff7e81d47fc
Martin Ionescu <martinionescu11@gmail.com>
2022-05-11 01:05:32 +0300

    added flyway

--- front/amadeusSports/src/app/service/match-table-loader.service.ts
+++ front/amadeusSports/src/app/service/match-table-loader.service.ts
@@ -2,11 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { MatchDto } from '../dto/MatchDto';
 import { PrincipalComponentLoaderService } from './principal-component-loader.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-const ELEMENT_DATA: MatchDto[] = [
-  new MatchDto(1, 'PSG', 'Saint Etienne', '3-0', 'football', 'Ligue 1', 'asd', false),
-  new MatchDto(2, 'PSG', 'Saint Etienne', '2-1', 'football', 'Ligue 1', 'asd', false),
-  new MatchDto(3, 'PSG', 'Saint Etienne', '3-0', 'football', 'Ligue 1', 'asd', false)
-];
+
 const URL = "http://localhost:8080/AmadeusSports"
 
 @Injectable({
@@ -15,7 +11,6 @@ const URL = "http://localhost:8080/AmadeusSports"
 export class MatchTableLoaderService{
 
   public matchesLoaded = new EventEmitter<MatchDto[]>();
-  matches: MatchDto[] = ELEMENT_DATA;
   sendMatches: MatchDto[] = [];
   constructor(private loader: PrincipalComponentLoaderService, private http: HttpClient) { }
 
