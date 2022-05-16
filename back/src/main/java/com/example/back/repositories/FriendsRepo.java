a4787435c00951f437102a59c669dc260991697e
Buzera Tiberiu <tibi.alex@yahoo.com>
2022-05-16 22:56:17 +0300

    Remove and get all friends

--- back/src/main/java/com/example/back/repositories/FriendsRepo.java
+++ back/src/main/java/com/example/back/repositories/FriendsRepo.java
@@ -3,8 +3,10 @@ package com.example.back.repositories;
 import com.example.back.models.entities.Friends;
 import com.example.back.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,6 @@ public interface FriendsRepo extends JpaRepository<Friends, Long> {
 
     Optional<Friends> findByMeAndFriends(User me, User friend);
 
+    @Query("select f.friends from Friends f where f.me = ?1")
+    ArrayList<User> findAllFriends(User me);
 }
