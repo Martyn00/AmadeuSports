e78465d88c414b2815008ac296f99b456972e2b7
Stefan Nedelcu <stefanedelcu2000@yahoo.ro>
2022-05-17 09:08:41 +0300

    Direct in main

--- back/src/main/resources/db/migration/V2__addstarterentities.sql
+++ back/src/main/resources/db/migration/V2__addstarterentities.sql
@@ -9,5 +9,26 @@ INSERT INTO team (id,country, name) values
     (4,'Romania', 'FCSB');
 INSERT INTO team (id,country, name) values
     (5,'Romania', 'FC U Craiova');
+INSERT INTO team (id,country, name) values
+    (6,'Spnia', 'FC Barcelona');
+INSERT INTO team (id,country, name) values
+    (7,'Franta', 'PSG');
+INSERT INTO team (id,country, name) values
+    (8,'Ungaria', 'FC Kurtoskolack');
+INSERT INTO team (id,country, name) values
+    (9,'Spania', 'Atletico Madrid');
+
 INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
-    (6, 'super', 1, '0-3', '2022-05-11 21:00:00', 4, 5);
+    (10, 'super', 1, '???', LOCALTIMESTAMP(), 4, 5);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (11, 'super', 0, '1-1', DATE_SUB(LOCALTIMESTAMP(), INTERVAL 1 DAY), 7, 8);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (12, 'super', 1, '???', DATE_ADD(LOCALTIMESTAMP(), INTERVAL 1 DAY), 5, 7);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (13, 'super', 0, '2-3', DATE_SUB(DATE_SUB(LOCALTIMESTAMP(), INTERVAL 1 DAY), INTERVAL 1 DAY), 9, 4);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (14, 'super', 1, '???', DATE_ADD(DATE_ADD(LOCALTIMESTAMP(), INTERVAL 1 DAY), INTERVAL 1 DAY), 5, 6);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (15, 'super', 0, '0-0', DATE_SUB(DATE_SUB(DATE_SUB(LOCALTIMESTAMP(), INTERVAL 1 DAY), INTERVAL 1 DAY), INTERVAL 1 DAY), 6, 8);
+INSERT INTO match_entity (id,data, is_upcoming, result, start_time, team1_id, team2_id ) values
+    (16, 'super', 1, '???', DATE_ADD(DATE_ADD(DATE_ADD(LOCALTIMESTAMP(), INTERVAL 1 DAY), INTERVAL 1 DAY), INTERVAL 1 DAY), 6, 9);
