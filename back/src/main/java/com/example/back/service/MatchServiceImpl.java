8eb60fdaaf02096a6a841b80e7caf3b36d12f186
Stefan Nedelcu <stefanedelcu2000@yahoo.ro>
2022-05-20 16:29:18 +0300

    .

--- back/src/main/java/com/example/back/service/MatchServiceImpl.java
+++ back/src/main/java/com/example/back/service/MatchServiceImpl.java
@@ -2,9 +2,13 @@ package com.example.back.service;
 
 import com.example.back.controllers.dto.MatchDto;
 import com.example.back.models.entities.MatchEntity;
+import com.example.back.models.entities.User;
 import com.example.back.repositories.MatchRepo;
+import com.example.back.repositories.UserRepo;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
