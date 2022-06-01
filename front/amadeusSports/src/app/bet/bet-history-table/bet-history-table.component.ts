51fcf361ff6180659053a10a3493d758c28a0146
Martin Ionescu <martinionescu11@gmail.com>
2022-06-01 16:16:43 +0300

    mesaj

--- front/amadeusSports/src/app/bet/bet-history-table/bet-history-table.component.ts
+++ front/amadeusSports/src/app/bet/bet-history-table/bet-history-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BetDto } from 'src/app/dto/BetDto';
 import { BetService } from 'src/app/service/bet.service';
+import { LeaguesService } from 'src/app/service/leagues.service';
+import { TeamsService } from 'src/app/service/teams.service';
 
 @Component({
   selector: 'app-bet-history-table',
@@ -9,7 +11,7 @@ import { BetService } from 'src/app/service/bet.service';
 })
 export class BetHistoryTableComponent implements OnInit {
 
-  constructor(private betService: BetService) { }
+  constructor(private betService: BetService,private teamService: TeamsService,private leagueService: LeaguesService) { }
   dataSource!: BetDto[];
   displayedColumns: string[] = ['time', 'team1', 'team2', 'result', 'league', 'status', 'coins'];
   ngOnInit(): void {
@@ -20,13 +22,21 @@ export class BetHistoryTableComponent implements OnInit {
   }
 
   favoritesTeam1(element: BetDto) {
-
+    this.teamService.getTeamByName(element.team1).subscribe((team) => {
+      this.teamService.changeFavoriteStateTeam(team);
+    });
   }
 
   favoritesTeam2(element: BetDto) {
-
+    this.teamService.getTeamByName(element.team2).subscribe((team) => {
+      this.teamService.changeFavoriteStateTeam(team);
+    });
   }
+
   favoritesLeague(element: BetDto) {
+    this.leagueService.getleagueByName(element.league).subscribe((league) => {
+      this.leagueService.changeFavoriteStateLeague(league);
+    });
 
   }
 }
--- front/amadeusSports/src/app/bet/bet-table/bet-table.component.ts
+++ front/amadeusSports/src/app/bet/bet-table/bet-table.component.ts
@@ -3,6 +3,8 @@ import { BetDto } from 'src/app/dto/BetDto';
 import { UserBetChoiceDto } from 'src/app/dto/UserBetChoiceDto';
 import { UserDto } from 'src/app/dto/UserDto';
 import { BetService } from 'src/app/service/bet.service';
+import { LeaguesService } from 'src/app/service/leagues.service';
+import { TeamsService } from 'src/app/service/teams.service';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -12,7 +14,8 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class BetTableComponent implements OnInit {
 
-  constructor(private betService: BetService, private userService: UserService) { }
+  constructor(private betService: BetService, private userService: UserService,
+    private teamService: TeamsService, private leagueService: LeaguesService) { }
   dataSource!: BetDto[];
   displayedColumns: string[] = ['time', 'team1', 'team2', 'result', 'league', 'bet', 'status', 'coins'];
   user!: UserDto;
@@ -27,13 +30,21 @@ export class BetTableComponent implements OnInit {
   }
 
   favoritesTeam1(element: BetDto) {
-
+    this.teamService.getTeamByName(element.team1).subscribe((team) => {
+      this.teamService.changeFavoriteStateTeam(team);
+    });
   }
 
   favoritesTeam2(element: BetDto) {
-
+    this.teamService.getTeamByName(element.team2).subscribe((team) => {
+      this.teamService.changeFavoriteStateTeam(team);
+    });
   }
+  
   favoritesLeague(element: BetDto) {
+    this.leagueService.getleagueByName(element.league).subscribe((league) => {
+      this.leagueService.changeFavoriteStateLeague(league);
+    });
 
   }
 
